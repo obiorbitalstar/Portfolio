7de1a301456e8646bcb8eaebd1b12f68ed7046d5
ObiOrbitalStar <obiorbitalstar@gmail.com>
2020-04-04 00:19:00 +0300

    fixed stuff

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 'use strict';
 
-require('dotenv').config();
 const express = require('express');
 const app = express();
 
@@ -8,18 +7,18 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./public'));
 
-app.get('/hello', (request, response) => {
-  response.status(200).send('Hello');
-});
+// app.get('/hello', (request, response) => {
+//   response.status(200).send('Hello');
+// });
 
-app.get('/data', (request, response) => {
-  let airplanes = {
-    departure: Date.now(),
-    canFly: true,
-    pilot: 'Well Trained',
-  };
-  response.status(200).json(airplanes);
-});
+// app.get('/data', (request, response) => {
+//   let airplanes = {
+//     departure: Date.now(),
+//     canFly: true,
+//     pilot: 'Well Trained',
+//   };
+//   response.status(200).json(airplanes);
+// });
 
 app.use('*', (request, response) => response.send('Sorry, that route does not exist.'));
 
